2080b68659a32d5de5f1cf50a49932a10428e9ba
Luca Schlomski <luca.schlomski@pickware.de>
2022-07-13 19:43:36 +0200

    change Procfile

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: true,
-  port: process.env.PORT || 3000
+  socketMode: true
 })
 
 
